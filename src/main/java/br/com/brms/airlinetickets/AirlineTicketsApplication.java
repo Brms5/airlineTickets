1b3eb7fa3db9b5a903bd202fc3ff3b9f2b4fecc0
brms5 <moreirasbr@gmail.com>
2024-07-28 11:25:36 -0300

    initial structure

--- src/main/java/br/com/brms/airlinetickets/AirlineTicketsApplication.java
+++ src/main/java/br/com/brms/airlinetickets/AirlineTicketsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AirlineticketsApplication {
+public class AirlineTicketsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AirlineticketsApplication.class, args);
+		SpringApplication.run(AirlineTicketsApplication.class, args);
 	}
 
 }
