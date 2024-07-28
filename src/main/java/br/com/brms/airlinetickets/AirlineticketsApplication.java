2d4ac4d08367e1c2238238a68e8aadf834ab24d8
brms5 <moreirasbr@gmail.com>
2024-07-27 22:20:49 -0300

    init commit

---
+++ src/main/java/br/com/brms/airlinetickets/AirlineticketsApplication.java
@@ -0,0 +1,13 @@
+package br.com.brms.airlinetickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineticketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brms/airlinetickets/AirlineticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brms.airlinetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
